1fbefa014b11190b749f52bdba89935e94fbbdf0
Jayce <jstephe3@xula.edu>
2025-11-09 23:49:39 -0600

    Rename scraper variable in driver.py

--- driver.py
+++ driver.py
@@ -8,13 +8,13 @@ from soupMK import SoupMaker
 
 def main():
     url = "https://www.amazon.com/s?k="
-    scraper = SoupMaker(url, headers=None)
+    soupM = SoupMaker(url, headers=None)
 
     usrinput = input("Enter the product you want to search for on Amazon: ")
     url += usrinput.replace(" ", "+") #Replace spaces with '+' for URL encoding
 
     
-    soup = scraper.makeSoup(url)
+    soup = soupM.makeSoup(url)
 
     
 
